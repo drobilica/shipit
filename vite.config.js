b9f82a92bb6cba90b106c1f8292808e0ad6324d2
drobilica <dusan@drobilica.com>
2024-01-03 01:11:39 +0100

    Commented out the export default in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
 // vite.config.js
-export default {
-    base: process.env.NODE_ENV === 'production' ? '/trade/' : '/'
-}
+// export default {
+//     base: process.env.NODE_ENV === 'production' ? '/trade/' : '/'
+// }
